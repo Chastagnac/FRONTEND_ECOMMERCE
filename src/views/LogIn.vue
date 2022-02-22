0e0c54b0e3cb022833d36f212600369311eb30f8
ELRida <91136483+ELRida@users.noreply.github.com>
2022-02-22 15:46:14 +0100

    Update LogIn.vue

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -139,7 +139,7 @@ export default {
           axios.defaults.headers.common["Authorization"] = "Token " + token;
          
           localStorage.setItem("token", token);
-          this.toast_affiche("Vous êtes connecté","is-success");
+          this.toast_affiche("Vous êtes connecté","is-info");
           const toPath = this.$route.query.to || "/";
           this.$router.push(toPath);
         })
